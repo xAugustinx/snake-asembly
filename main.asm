66b6ddb0aaa63ab10f084a2ee2a52b8a6eb33e45
𝙰𝚞𝚐𝚞𝚜𝚝𝚒𝚗 <xaugustinx12@gmail.com>
2025-11-30 20:10:23 +0100

    Add files via upload

--- main.asm
+++ main.asm
@@ -2,85 +2,145 @@
 .intel_syntax noprefix
 
 .section .bss
+    wszystkieElementyWenza: .space 64
+    kierunekWenza: .space 1
+    plansza: .space 73
+    czyKoniec: .space 1
+    japko: .space 1
     input: .space 1
-    plansza: .space 71
-    japki: .space 2
-    wonszPlace: .space 128
-    ##bools
-    czyKoniecGry: .space 1
-    czyUsunacOstatniElement: .space 1
-    #Parzyste + 0 to Y 
-    #Nieparzyste to X
-    tymczasowaPozycjaWensza: .space 2
-    
 
+
+    licznikTestowyMeow: .space 1
 
 .section .data
+
 consoleClear:
         .ascii "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-enter:
-    .ascii "\n"
 .section .text
+
 _start:
-    ##PLANSZA NA ZERO + granice
-    MOV eax, 0
-    lea rbx, [rip + plansza]
+    MOV byte ptr [rip + licznikTestowyMeow], 49
 
-    planszaNaZero:
-    MOV byte ptr [rbx + rax], 35
-    ADD rax, 1
-    CMP rax, 71
-    JNE planszaNaZero
-    
-    MOV rax, 8
+    ##USTAWIANIE WĘŻA NA ZERO
+    LEA rbx, [rip + wszystkieElementyWenza]
+    MOV rax, 0
 
-    granicePlanszy:
-    MOV byte ptr [rbx + rax], 10
-    ADD rax, 9
-    CMP rax, 71
-    JNE granicePlanszy
-
-
-    ##WONSZ NA ZERO
-    MOV eax, 4
-    lea rbx, [rip + wonszPlace]
-
-    wonszNaZero:
+    ustawianieWenszaNaZero:
     MOV byte ptr [rbx + rax], 255
     ADD rax, 1
-    CMP rax, 128
-    JNE wonszNaZero
+    CMP rax, 64
+    JNE ustawianieWenszaNaZero
 
-    MOV byte ptr [rip + wonszPlace],5
-    MOV byte ptr [rip + wonszPlace+1],5
-    MOV byte ptr [rip + wonszPlace+2],5
-    MOV byte ptr [rip + wonszPlace+3],6
+    MOV byte ptr [rip + kierunekWenza],20
 
-    #USTAWIE PSEUDO ZMIENNYCH
-    MOV byte ptr [rip + input], 1
-    MOV byte ptr [rip + japki], 3
-    MOV byte ptr [rip + japki + 1], 3
-    MOV byte ptr [rip + czyKoniecGry], 0
-    MOV byte ptr [rip + czyUsunacOstatniElement], 0
+    ##Zmienne Dodatkowe
+    MOV byte ptr [rip + wszystkieElementyWenza],29
+    MOV byte ptr [rip + wszystkieElementyWenza+1],38
+    MOV byte ptr [rip + wszystkieElementyWenza+2],47
+    MOV byte ptr [rip + wszystkieElementyWenza+3],56
 
-    petlaGry:
-        jmp zbieranieInputu
-        koniecZbieraniaInputu:
+    mainPetla:
 
-        jmp zmienianieKierunkuWensza
-        koniecZmienianiaKierunkuWensza:
 
-        jmp wypisywanieZawartosciTablicy
-        koniecWypisywaniaZawartosciTablicy:
+    jmp wczytywanieZawartosciDoTablicy
+    koniecWczytywaniaZawartosciDoTablicy:
 
-        CMP byte ptr [rip + czyKoniecGry],1
-    JNE petlaGry
+    jmp wypisywanieZawartosciTablicy
+    koniecWypisywaniaZawartosciTablicy:
+
+    jmp zbieranieInputu
+    koniecZbieraniaInputu:
+
+    jmp dodanieNowegoElementuDoWensza
+    koniecDodaniaNowegoElementuDoWensza:
+
+    jmp usuniecieOstatniegoElementu
+    koniecUsunieciaOstatniegoElementu:
+
+    jmp mainPetla
+
+    jmp koniec
+
+dodanieNowegoElementuDoWensza:
+    MOV rax, 62
+    LEA rbx, [rip + wszystkieElementyWenza]
+    miejsceNaNowyElement:
+    MOV al, byte ptr [rbx + rax]
+    MOV byte ptr [rbx + rax +1], al
+
+    ADD rax, -1
+    CMP rax, -1
+    JNE miejsceNaNowyElement
+
+
+    MOV al, byte ptr [rip + kierunekWenza]
+    ADD byte ptr [rip + wszystkieElementyWenza], al
+
     
-    mov rax, 60
-    mov rdi, 60
-    syscall
+    jmp koniecDodaniaNowegoElementuDoWensza
+
+usuniecieOstatniegoElementu:
+    MOV rax, -1
+    LEA rbx, [rip + wszystkieElementyWenza]
+
+    szukanieOstatniegoElementu33:
+    ADD rax, 1
+    CMP byte ptr [rbx + rax], 255
+    JNE szukanieOstatniegoElementu33
+
+    MOV byte ptr [rbx + rax -1], 255
+
+    ##licznik testowy
+    ADD byte ptr [rip + licznikTestowyMeow], 1
+
+    mov rax, 1
+	mov rdi, 1
+	lea rsi, [rip + licznikTestowyMeow]
+	mov rdx, 1
+	syscall
+
+    jmp koniecUsunieciaOstatniegoElementu
+wczytywanieZawartosciDoTablicy:
+    #ustawianie planszy na Zero
+    LEA rbx, [rip + plansza]
+
+    MOV rax, 0
+    ustawianiePlanszyNaZero:
+    MOV byte ptr [rbx + rax], 35
+
+    ADD rax, 1
+    CMP rax, 72
+    JNE ustawianiePlanszyNaZero
+    ##dodanie  \n
+    MOV rax, 0
+    dodanieN:
+    MOV byte ptr [rbx + rax],10
+    ADD rax, 9
+    CMP rax, 72
+    JNE dodanieN
+
+    
+
+    MOV rax, 0
+    MOV rcx, 0
+    MOV rdx, 0
+
+    LEA rbx, [rip + wszystkieElementyWenza]
+
+    ustawianieWenszaNaPlansze:
+    ##MEOW MEOW
 
 
+    MOVZX rcx, byte ptr [rbx + rax]
+    LEA rdx, [rip + plansza]
+
+    MOV byte ptr [rdx + rcx], 79
+
+    ADD rax, 1
+    CMP byte ptr [rbx + rax], 255
+    JNE ustawianieWenszaNaPlansze
+
+    jmp koniecWczytywaniaZawartosciDoTablicy
 wypisywanieZawartosciTablicy:
     mov rax, 1
     mov rdi, 1
@@ -88,17 +148,10 @@ wypisywanieZawartosciTablicy:
     mov rdx, 20
     syscall
 
-
     mov rax, 1
     mov rdi, 1
     lea rsi, [rip + plansza]
-    mov rdx, 71
-    syscall
-
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, enter
-    mov rdx, 1
+    mov rdx, 73
     syscall
 
     jmp koniecWypisywaniaZawartosciTablicy
@@ -107,48 +160,54 @@ zbieranieInputu:
     MOV rax, 0
     MOV rdi, 0
     LEA rsi, [rip + input]
-    MOV rdx, 1
+    MOV rdx, 2
     syscall
-    jmp koniecZbieraniaInputu
+    
+    MOV al, byte ptr [rip + wszystkieElementyWenza]
+    MOV byte ptr [rip + kierunekWenza], al
 
-
-zmienianieKierunkuWensza:
-    MOV al, byte ptr [rip + wonszPlace]
-    MOV byte ptr [rip + tymczasowaPozycjaWensza], al
-
-    MOV al, byte ptr [rip + wonszPlace +1]
-    MOV byte ptr [rip + tymczasowaPozycjaWensza + 1], al
+    ##  MOV byte ptr [rip + kierunekWenza], byte ptr [rip + wszystkieElementyWenza]
 
     CMP byte ptr [rip + input], 119
-    JNE klawiszW
+    JE klawiszW
 
     CMP byte ptr [rip + input], 115
-    JNE klawiszS
+    JE klawiszS
 
     CMP byte ptr [rip + input], 97
-    JNE klawiszA
+    JE klawiszA
 
     CMP byte ptr [rip + input], 100
-    JNE klawiszD
+    JE klawiszD
 
     kontynulujPoSprawdzeniuKlawisza:
 
-    jmp koniecZmienianiaKierunkuWensza
 
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rip + input+1]
+    mov rdx, 1
+    syscall
+
+
+    jmp koniecZbieraniaInputu
 klawiszW:
-    ADD byte ptr [rip + tymczasowaPozycjaWensza], -1
+    MOV byte ptr [rip + kierunekWenza], -9
     jmp kontynulujPoSprawdzeniuKlawisza
 
 klawiszS:
-    ADD byte ptr [rip + tymczasowaPozycjaWensza], 1
+    MOV byte ptr [rip + kierunekWenza], 9
     jmp kontynulujPoSprawdzeniuKlawisza
 
 klawiszA:
-    ADD byte ptr [rip + tymczasowaPozycjaWensza+1], -1
+    MOV byte ptr [rip + kierunekWenza], -1
     jmp kontynulujPoSprawdzeniuKlawisza
 
 klawiszD:
-    ADD byte ptr [rip + tymczasowaPozycjaWensza+1], 1
+    MOV byte ptr [rip + kierunekWenza], 1
     jmp kontynulujPoSprawdzeniuKlawisza
 
-
+koniec:
+    MOV rax, 60
+    MOV rdi, 60
+    syscall
